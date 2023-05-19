5858de5968aac52f1b4435616f5931e6c1d1db69
dxliang001 <dongxinliang6@gmail.com>
2023-05-19 12:41:33 -0400

    system.out.println to logger.info

--- Solvd_Hosptial/src/main/java/com/solvd/Hospital/Classes/AdministrativeStaff.java
+++ Solvd_Hosptial/src/main/java/com/solvd/Hospital/Classes/AdministrativeStaff.java
@@ -2,15 +2,20 @@ package com.solvd.Hospital.Classes;
 import com.solvd.Hospital.Enums.Gender;
 import com.solvd.Hospital.Interface.IMaintainable;
 
+import com.solvd.Hospital.Main;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class AdministrativeStaff extends Employee implements IMaintainable {
 
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public AdministrativeStaff(String name, Gender gender, int age, int experience, Department department, String jobTitle) {
         super(name, gender, age, experience, department, jobTitle);
     }
 
     @Override
     public void work() {
-        System.out.println("The administrative staff is handling paperwork and managing resources.");
+        logger.info("The administrative staff is handling paperwork and managing resources.");
     }
 
     @Override
